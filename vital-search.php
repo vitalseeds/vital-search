a898ea9f9146263192d4202ffed1e85b720e4f4d
tombola <tombola@github>
2026-01-26 15:26:07 +0000

    Bust cache for search js

--- vital-search.php
+++ vital-search.php
@@ -18,7 +18,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-define('VITAL_SEARCH_VERSION', '1.0.0');
+define('VITAL_SEARCH_VERSION', filemtime(__DIR__ . '/js/vital-search.js'));
 define('VITAL_SEARCH_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('VITAL_SEARCH_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('VITAL_SEARCH_CRON_HOOK', 'vital_search_export');
